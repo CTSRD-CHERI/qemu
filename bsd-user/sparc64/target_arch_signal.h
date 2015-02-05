4b42d28d8f84163b675931b327efc0154ee3bc74
Sean Bruno <sbruno@freebsd.org>
2015-02-05 15:52:58 -0800

    Add support for futimens(8) and utimensat(8) syscalls intruduced in freebsd at:
    https://svnweb.freebsd.org/base?view=revision&revision=277610
    
    - Add target_to_host_bitmask() for the utimensat() file flags arg.
    - Add entries for both futimens() and utimensat() for -strace option.
    - TARGET_FREEBSD_NR_MAXSYSCALL must be greater than last system call
    number.
    
    Submitted-by: Kevin Day <kevin@your.org>
    Signed-off-by: Sean Bruno <sbruno@FreeBSD.org>
    Signed-off-by: Stacy Son <sson@FreeBSD.org>
    [imp integrated several changes by sbruno and sson and a tweak by kevin]
    Signed-off-by: Warner Losh<imp@FreeBSD.org>

--- bsd-user/sparc64/target_arch_signal.h
+++ bsd-user/sparc64/target_arch_signal.h
@@ -1,6 +1,7 @@
 /*
  *  sparc64 dependent signal definitions
  *
+ *    Copyright (c) 2015 Stacey D. Son
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,6 +21,16 @@
 
 #include "cpu.h"
 
+#define UREG_O0     16
+#define UREG_O1     17
+#define UREG_O2     18
+#define UREG_O3     19
+#define UREG_O4     20
+#define UREG_O5     21
+#define UREG_O6     22
+#define UREG_O7     23
+
+
 /* Size of the signal trampolin code placed on the stack. */
 /* #define TARGET_SZSIGCODE    (0) */      /* XXX to be added. */
 
@@ -33,8 +44,29 @@ struct target_sigcontext {
     /* to be added */
 };
 
-typedef struct target_mcontext {
-} target_mcontext_t;
+/* See sparc64/include/ucontext.h */
+struct target_mcontext {
+    uint64_t    mc_global[8];
+    uint64_t    mc_out[8];
+    uint64_t    mc_local[8];
+    uint64_t    mc_in[8];
+    uint32_t    mc_fp[64];
+} __aligned(64);
+
+#define _mc_flags   mc_global[0]
+#define _mc_sp      mc_out[6]
+#define _mc_fprs    mc_local[0]
+#define _mc_fsr     mc_local[1]
+#define _mc_gsr     mc_local[2]
+#define _mc_tnpc    mc_in[0]
+#define _mc_tpc     mc_in[1]
+#define _mc_tstate  mc_in[2]
+#define _mc_y       mc_in[4]
+#define _mc_wstate  mc_in[5]
+
+#define _MC_VERSION 1L
+
+typedef struct target_mcontext target_mcontext_t;
 
 typedef struct target_ucontext {
     target_sigset_t   uc_sigmask;
@@ -64,6 +96,8 @@ static inline abi_long set_sigtramp_args(CPUSPARCState *regs,
         struct target_sigaction *ka)
 {
     /* XXX */
+    printf ("incomplete %s: %s:%d\n", __func__, __FILE__, __LINE__);
+    exit(-1);
     return -TARGET_EOPNOTSUPP;
 }
 
@@ -71,22 +105,82 @@ static inline abi_long set_sigtramp_args(CPUSPARCState *regs,
 static inline abi_long get_mcontext(CPUSPARCState *regs,
         target_mcontext_t *mcp, int flags)
 {
-    /* XXX */
-    return -TARGET_EOPNOTSUPP;
+    trap_state *tsptr = cpu_tsptr(regs);
+
+    mcp->_mc_flags = _MC_VERSION;
+    mcp->mc_global[1] = tswapal(regs->gregs[1]);
+    mcp->mc_global[2] = tswapal(regs->gregs[2]);
+    mcp->mc_global[3] = tswapal(regs->gregs[3]);
+    mcp->mc_global[4] = tswapal(regs->gregs[4]);
+    mcp->mc_global[5] = tswapal(regs->gregs[5]);
+    mcp->mc_global[6] = tswapal(regs->gregs[6]);
+    if (flags & TARGET_MC_GET_CLEAR_RET) {
+        mcp->mc_out[0] = 0;
+        mcp->mc_out[1] = 0;
+    } else {
+        mcp->mc_out[0] = tswapal(regs->regwptr[UREG_O0]);
+        mcp->mc_out[1] = tswapal(regs->regwptr[UREG_O1]);
+    }
+    mcp->mc_out[2] = tswapal(regs->regwptr[UREG_O2]);
+    mcp->mc_out[3] = tswapal(regs->regwptr[UREG_O3]);
+    mcp->mc_out[4] = tswapal(regs->regwptr[UREG_O4]);
+    mcp->mc_out[5] = tswapal(regs->regwptr[UREG_O5]);
+    mcp->mc_out[6] = tswapal(regs->regwptr[UREG_O6]);
+    mcp->mc_out[7] = tswapal(regs->regwptr[UREG_O7]);
+    mcp->_mc_fprs = tswapal(regs->fprs);
+    mcp->_mc_fsr = tswapal(regs->fsr);
+    mcp->_mc_gsr = tswapal(regs->gsr);
+    mcp->_mc_tnpc = tswapal(tsptr->tnpc);
+    mcp->_mc_tpc = tswapal(tsptr->tpc);
+    mcp->_mc_tstate = tswapal(tsptr->tstate);
+    mcp->_mc_y = tswapal(regs->y);
+
+    /* XXX FPRS_FEF flag and floating point regs */
+
+    return 0;
 }
 
 /* Compare to sparc64/space64/machdep.c set_mcontext() */
 static inline abi_long set_mcontext(CPUSPARCState *regs,
         target_mcontext_t *mcp, int srflag)
 {
-    /* XXX */
-    return -TARGET_EOPNOTSUPP;
+    trap_state *tsptr = cpu_tsptr(regs);
+
+    regs->gregs[1] = tswapal(mcp->mc_global[1]);
+    regs->gregs[2] = tswapal(mcp->mc_global[2]);
+    regs->gregs[3] = tswapal(mcp->mc_global[3]);
+    regs->gregs[4] = tswapal(mcp->mc_global[4]);
+    regs->gregs[5] = tswapal(mcp->mc_global[5]);
+    regs->gregs[6] = tswapal(mcp->mc_global[6]);
+
+    regs->regwptr[UREG_O0] = tswapal(mcp->mc_out[0]);
+    regs->regwptr[UREG_O1] = tswapal(mcp->mc_out[1]);
+    regs->regwptr[UREG_O2] = tswapal(mcp->mc_out[2]);
+    regs->regwptr[UREG_O3] = tswapal(mcp->mc_out[3]);
+    regs->regwptr[UREG_O4] = tswapal(mcp->mc_out[4]);
+    regs->regwptr[UREG_O5] = tswapal(mcp->mc_out[5]);
+    regs->regwptr[UREG_O6] = tswapal(mcp->mc_out[6]);
+    regs->regwptr[UREG_O7] = tswapal(mcp->mc_out[7]);
+
+    regs->fprs = tswapal(mcp->_mc_fprs);
+    regs->fsr = tswapal(mcp->_mc_fsr);
+    regs->gsr = tswapal(mcp->_mc_gsr);
+    tsptr->tnpc = tswapal(mcp->_mc_tnpc);
+    tsptr->tpc = tswapal(mcp->_mc_tpc);
+    tsptr->tstate = tswapal(mcp->_mc_tstate);
+    regs->y = tswapal(mcp->_mc_y);
+
+    /* XXX FPRS_FEF flag and floating point regs */
+
+    return 0;
 }
 
 static inline abi_long get_ucontext_sigreturn(CPUSPARCState *regs,
         abi_ulong target_sf, abi_ulong *target_uc)
 {
     /* XXX */
+    printf ("incomplete %s: %s:%d\n", __func__, __FILE__, __LINE__);
+    exit(-1);
     *target_uc = 0;
     return -TARGET_EOPNOTSUPP;
 }
