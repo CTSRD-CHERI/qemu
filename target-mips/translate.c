ca633a36f2a679ed7664063a1157efdc6be636d3
Stacey Son <sson@FreeBSD.org>
2016-05-05 17:14:35 +0000

    Bug Fix: Add missing 'break'.

--- target-mips/translate.c
+++ target-mips/translate.c
@@ -10802,6 +10802,7 @@ static void gen_cp2 (DisasContext *ctx, uint32_t opc, int r16, int r11, int r6)
                     opn = "c1operand";
                     goto invalid;
                 }
+		break;
 
             default:
                 opn = "c2operand";
