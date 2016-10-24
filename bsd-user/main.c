8fdc2e2d4d25984ec887cb04b09da35ca1de2680
Sean Bruno <sbruno@freebsd.org>
2016-10-24 10:28:22 -0600

    Likely, this commit breaks literally everything.  Common cpu functions have been moved to the root of build tree and the fixups that sson implemented are probably gone.  I'll have to test this a bit before pushing to fbsd ports
    xxxxxxxxxxx

--- bsd-user/main.c
+++ bsd-user/main.c
@@ -108,10 +108,7 @@ static pthread_mutex_t exclusive_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t exclusive_cond = PTHREAD_COND_INITIALIZER;
 static pthread_cond_t exclusive_resume = PTHREAD_COND_INITIALIZER;
 static int pending_cpus;
-static int sleeping_cpus;
-static CPUState *exclusive_cpu;
 
-/* Make sure everything is in a consistent state for calling fork(). */
 void fork_start(void)
 {
     qemu_mutex_lock(&tcg_ctx.tb_ctx.tb_lock);
@@ -145,102 +142,6 @@ void fork_end(int child)
         qemu_mutex_unlock(&tcg_ctx.tb_ctx.tb_lock);
     }
 
-}
-
-/*
- * Wait for pending exclusive operations to complete.  The exclusive lock
- * must be held.
- */
-static inline void exclusive_idle(void)
-{
-    while (exclusive_cpu != NULL && exclusive_cpu != thread_cpu) {
-	if (thread_cpu->running)
-		sleeping_cpus++;
-	pthread_cond_signal(&exclusive_cond);
-        pthread_cond_wait(&exclusive_resume, &exclusive_lock);
-	if (thread_cpu->running)
-		sleeping_cpus--;
-    }
-}
-
-/* Start an exclusive operation.  Must only be called outside of cpu_exec. */
-void start_exclusive(void)
-{
-    CPUState *other_cpu;
-
-    pthread_mutex_lock(&exclusive_lock);
-    exclusive_idle();
-    
-    exclusive_cpu = thread_cpu;
-
-    do {
-	    pending_cpus = 1;
-	    /* Make all other cpus stop executing. */
-	    CPU_FOREACH(other_cpu) {
-		    if (other_cpu != thread_cpu && other_cpu->running) {
-			    pending_cpus++;
-			    cpu_exit(other_cpu);
-		    }
-	    }
-	    if ((pending_cpus - sleeping_cpus) > 1) {
-		    pthread_cond_wait(&exclusive_cond, &exclusive_lock);
-	    } else
-		    break;
-    } while (1);
-    
-}
-
-/* Finish an exclusive operation. */
-void end_exclusive(void)
-{
-    pending_cpus = 0;
-    exclusive_cpu = NULL;
-    pthread_cond_broadcast(&exclusive_resume);
-    pthread_mutex_unlock(&exclusive_lock);
-}
-
-/* Wait for exclusive ops to finish, and begin cpu execution. */
-void cpu_exec_start(CPUState *cpu)
-{
-    pthread_mutex_lock(&exclusive_lock);
-    exclusive_idle();
-    cpu->running = true;
-    pthread_mutex_unlock(&exclusive_lock);
-}
-
-/* Mark cpu as not excuting, and release pending exclusive ops. */
-void cpu_exec_end(CPUState *cpu)
-{
-    pthread_mutex_lock(&exclusive_lock);
-    cpu->running = false;
-    if (pending_cpus > 1) {
-        pending_cpus--;
-        if (pending_cpus == 1) {
-            pthread_cond_signal(&exclusive_cond);
-        }
-    }
-    exclusive_idle();
-    /*
-     * This is a hack, we gotta keep an exclusive section while handling signals
-     * but it will returns using longjmp, so we never have the opportunity to
-     * call end_exclusive()
-     */
-    if (exclusive_cpu == thread_cpu) {
-	    pending_cpus--;
-	    exclusive_cpu = NULL;
-	    pthread_cond_broadcast(&exclusive_resume);
-    }
-    pthread_mutex_unlock(&exclusive_lock);
-}
-
-void cpu_list_lock(void)
-{
-    pthread_mutex_lock(&cpu_list_mutex);
-}
-
-void cpu_list_unlock(void)
-{
-    pthread_mutex_unlock(&cpu_list_mutex);
 }
 
 void cpu_loop(CPUArchState *env)
