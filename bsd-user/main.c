05cb892f9edb103ce293c9bf0f7d590c73d54f96
Sean Bruno <sbruno@freebsd.org>
2016-12-13 15:05:17 -0700

    Reenable missing init of the cpu list lock

--- bsd-user/main.c
+++ bsd-user/main.c
@@ -265,7 +265,7 @@ int main(int argc, char **argv)
 
     error_init(argv[0]);
     module_call_init(MODULE_INIT_TRACE);
-    //qemu_init_cpu_list();
+    qemu_init_cpu_list();
     module_call_init(MODULE_INIT_QOM);
 
     if ((envlist = envlist_create()) == NULL) {
