1d114bac93792b0d16d6d9610a4d6d522f3d02a4
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-04-04 14:34:17 +0100

    Fix -Wformat warnings on MacOS

--- target/mips/op_helper.c
+++ target/mips/op_helper.c
@@ -101,9 +101,10 @@ static inline void do_raise_c2_exception(CPUMIPSState *env, uint16_t cause,
          env->active_tc.PCC.cr_base, env->active_tc.PCC.cr_offset, pc,
          env->active_tc.PC); */
     qemu_log_mask(CPU_LOG_INSTR | CPU_LOG_INT, "C2 EXCEPTION: cause=%d(%s) "
-       "reg=%d PCC=0x%016lx + 0x%016lx -> 0x%016lx PC=0x%016lx\n", cause,
-        causestr[cause], reg, env->active_tc.PCC.cr_base,
-        env->active_tc.PCC.cr_offset, pc, env->active_tc.PC);
+       "reg=%d PCC=0x%016" PRIx64 " + 0x%016" PRIx64 " -> 0x" TARGET_FMT_lx
+       " PC=0x" TARGET_FMT_lx "\n",
+       cause, causestr[cause], reg, env->active_tc.PCC.cr_base,
+       env->active_tc.PCC.cr_offset, pc, env->active_tc.PC);
     cpu_mips_store_capcause(env, reg, cause);
     env->active_tc.PC = pc;
     env->CP0_BadVAddr = pc;
@@ -119,9 +120,10 @@ static inline void do_raise_c0_exception(CPUMIPSState *env, uint16_t cause,
          badvaddr, env->active_tc.PCC.cr_base,
          env->active_tc.PCC.cr_offset, pc, env->active_tc.PC); */
     qemu_log_mask(CPU_LOG_INSTR | CPU_LOG_INT, "C0 EXCEPTION: cause=%d"
-        "badvaddr=0x%016lx PCC=0x%016lx + 0x%016lx -> 0x%016lx PC=0x%016lx\n",
-         cause, badvaddr, env->active_tc.PCC.cr_base,
-         env->active_tc.PCC.cr_offset, pc, env->active_tc.PC);
+        "badvaddr=0x%016" PRIx64 " PCC=0x%016" PRIx64 " + 0x%016" PRIx64
+        " -> 0x" TARGET_FMT_lx " PC=0x" TARGET_FMT_lx "\n",
+        cause, badvaddr, env->active_tc.PCC.cr_base,
+        env->active_tc.PCC.cr_offset, pc, env->active_tc.PC);
     env->active_tc.PC = pc;
     env->CP0_BadVAddr = badvaddr;
     do_raise_exception(env, cause, pc);
