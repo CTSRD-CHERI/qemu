CTSRD-CHERI/qemu

commit a0d765d9fa051f85d796097c8bf74da336c4fc7d

Fix CRAM

target/mips/op_helper_cheri.c
@@ -1197,6 +1197,7 @@ static void do_setbounds(bool must_be_exact, CPUMIPSState *env, uint32_t cd,
     const cap_register_t *cbp = get_readonly_capreg(&env->active_tc, cb);
     uint64_t cursor = cap_get_cursor(cbp);
     unsigned __int128 new_top = (unsigned __int128)cursor + length; // 65 bits
+    unsigned __int128 current_top = (unsigned __int128)cursor + cap_get_length(cbp); // 65 bits
     /*
      * CSetBounds: Set Bounds
      */
@@ -1207,9 +1208,9 @@ static void do_setbounds(bool must_be_exact, CPUMIPSState *env, uint32_t cd,
     } else if (cursor < cbp->cr_base) {
         do_raise_c2_exception(env, CP2Ca_LENGTH, cb);
     } else if (new_top > UINT64_MAX) {
-        /* cursor + rt overflowed */
+        /* We don't allow setbounds to create full address space caps */
         do_raise_c2_exception(env, CP2Ca_LENGTH, cb);
-    } else if (new_top > cap_get_top(cbp)) {
+    } else if (new_top > current_top) {
         do_raise_c2_exception(env, CP2Ca_LENGTH, cb);
     } else {
         cap_register_t result = *cbp;
@@ -1278,12 +1279,21 @@ target_ulong CHERI_HELPER_IMPL(cram)(CPUMIPSState *env, target_ulong len)
     // rt is set to a mask that can be used to align down addresses to a value
     // that is sufficiently aligned to set precise bounds for the nearest
     // representable length of rs (as obtained by CRoundArchitecturalPrecision).
-
 #ifdef CHERI_128
     // The mask used to align down is all ones followed by (required exponent
     // for compressed representation) zeroes
     uint32_t e = cc128_get_exponent(len);
-    return UINT64_MAX << e;
+    qemu_log("len = " TARGET_FMT_lx ", e = %d\n", len, e);
+    if (e != 0) {
+        // increment e by the width difference between internal and zero exponent (+3)
+        e += (CC128_BOT_WIDTH - CC128_BOT_INTERNAL_EXP_WIDTH);
+    }
+    target_ulong result = UINT64_MAX << e;
+    target_ulong rounded_len = helper_crap(env, len);
+    qemu_log("cram(" TARGET_FMT_lx "/" TARGET_FMT_lx ") result = " TARGET_FMT_lx ", e=%d\n", len, rounded_len, result, e);
+    target_ulong rounded_with_cram = (len + ~result) & result;
+    qemu_log("rounded_with_cram = " TARGET_FMT_lx "\n", rounded_with_cram);
+    return result;
 #else
     // For MAGIC128 and 256 everything is representable -> we can return all ones
     return UINT64_MAX;

target/mips/translate_cheri.c
@@ -1980,7 +1980,7 @@ static void gen_cp2 (DisasContext *ctx, uint32_t opc, int r16, int r11, int r6)
                 gen_store_gpr (t1, r16);
                 tcg_temp_free(t1);
                 tcg_temp_free(t0);
-                opn = "CRoundArchitecturalPrecision";
+                opn = "croundrepresetablelength";
                 break;
             }
             case OPC_CRAM_NI:   /* 0x11 << 6 */
@@ -1990,16 +1990,16 @@ static void gen_cp2 (DisasContext *ctx, uint32_t opc, int r16, int r11, int r6)
                 TCGv t1 = tcg_temp_new();
                 gen_load_gpr(t0, r11);
                 gen_helper_cram(t1, cpu_env, t0);
-                gen_store_gpr (t0, r16);
+                gen_store_gpr (t1, r16);
                 tcg_temp_free(t1);
                 tcg_temp_free(t0);
-                opn = "CRepresentableAlignmentMask";
+                opn = "crepresentablealignmentmask";
                 break;
             }
             case OPC_CSEALENTRY_NI:   /* 0x1d << 6 */
                 check_cop2x(ctx);
                 gen_helper_2_consti32(csealentry, r16, r11);
-                opn = "csealcode";
+                opn = "csealentry";
                 break;
             case OPC_CLOADTAGS_NI:   /* 0x1e << 6 */
                 check_cop2x(ctx);

commit 7d1671819f6a9d64bb4b1172a9c300d745b8ac7f

Fix assertion for non-offset zero csetbounds
Fixes #75

This bug was introduced in a0d765d9fa051f85d796097c8bf74da336c4fc7d. Cursor
should have been base.

target/mips/op_helper_cheri.c
@@ -1197,7 +1197,6 @@ static void do_setbounds(bool must_be_exact, CPUMIPSState *env, uint32_t cd,
     const cap_register_t *cbp = get_readonly_capreg(&env->active_tc, cb);
     uint64_t cursor = cap_get_cursor(cbp);
     unsigned __int128 new_top = (unsigned __int128)cursor + length; // 65 bits
-    unsigned __int128 current_top = (unsigned __int128)cursor + cap_get_length(cbp); // 65 bits
     /*
      * CSetBounds: Set Bounds
      */
@@ -1208,9 +1207,11 @@ static void do_setbounds(bool must_be_exact, CPUMIPSState *env, uint32_t cd,
     } else if (cursor < cbp->cr_base) {
         do_raise_c2_exception(env, CP2Ca_LENGTH, cb);
     } else if (new_top > UINT64_MAX) {
+        // TODO: special case for cheri128 full address space caps!
         /* We don't allow setbounds to create full address space caps */
         do_raise_c2_exception(env, CP2Ca_LENGTH, cb);
-    } else if (new_top > current_top) {
+    } else if (new_top > cap_get_top(cbp)) {
+        tcg_debug_assert(((unsigned __int128)cap_get_base(cbp) + cap_get_length(cbp)) <= UINT64_MAX && "csetbounds top currently limited to UINT64_MAX"); // 65 bits
         do_raise_c2_exception(env, CP2Ca_LENGTH, cb);
     } else {
         cap_register_t result = *cbp;