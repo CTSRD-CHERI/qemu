5b11d8e5be28031a3507f885237f17de89e57ec7
Igor Mammedov <imammedo@redhat.com>
2020-02-19 11:08:40 -0500

    vl.c: move -m parsing after memory backends has been processed
    It will be possible for main RAM to come from memory-backend
    and we should check that size specified in -m matches the size
    of the backend and [MachineState::]ram_size also matches
    backend's size.
    
    However -m parsing (set_memory_options()) happens before backends
    are intialized (object_create_delayed()) which complicates it.
    Consolidate set_memory_options() and assigning parsed results to
    current_machine after backends are initialized, so it would be
    possible access the initialized backend instance to compare
    sizes.
    
    This patch only consolidates scattered places touching ram_size
    within vl.c. And follow up patch will integrate backend handling
    to set_memory_options().
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20200219160953.13771-7-imammedo@redhat.com>

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -2721,6 +2721,14 @@ static void set_memory_options(uint64_t *ram_slots, ram_addr_t *maxram_size,
         exit(EXIT_FAILURE);
     }
 
+    if (!xen_enabled()) {
+        /* On 32-bit hosts, QEMU is limited by virtual address space */
+        if (ram_size > (2047 << 20) && HOST_LONG_BITS == 32) {
+            error_report("at most 2047 MB RAM can be simulated");
+            exit(1);
+        }
+    }
+
     loc_pop(&loc);
 }
 
@@ -3951,8 +3959,6 @@ int main(int argc, char **argv, char **envp)
     machine_class = select_machine();
     object_set_machine_compat_props(machine_class->compat_props);
 
-    set_memory_options(&ram_slots, &maxram_size, machine_class);
-
     os_daemonize();
 
     /*
@@ -4269,9 +4275,6 @@ int main(int argc, char **argv, char **envp)
     machine_opts = qemu_get_machine_opts();
     qemu_opt_foreach(machine_opts, machine_set_property, current_machine,
                      &error_fatal);
-    current_machine->ram_size = ram_size;
-    current_machine->maxram_size = maxram_size;
-    current_machine->ram_slots = ram_slots;
 
     /*
      * Note: uses machine properties such as kernel-irqchip, must run
@@ -4386,14 +4389,6 @@ int main(int argc, char **argv, char **envp)
 
     tpm_init();
 
-    if (!xen_enabled()) {
-        /* On 32-bit hosts, QEMU is limited by virtual address space */
-        if (ram_size > (2047 << 20) && HOST_LONG_BITS == 32) {
-            error_report("at most 2047 MB RAM can be simulated");
-            exit(1);
-        }
-    }
-
     blk_mig_init();
     ram_mig_init();
     dirty_bitmap_mig_init();
@@ -4438,6 +4433,12 @@ int main(int argc, char **argv, char **envp)
     if (cpu_option) {
         current_machine->cpu_type = parse_cpu_option(cpu_option);
     }
+
+    set_memory_options(&ram_slots, &maxram_size, machine_class);
+    current_machine->ram_size = ram_size;
+    current_machine->maxram_size = maxram_size;
+    current_machine->ram_slots = ram_slots;
+
     parse_numa_opts(current_machine);
 
     if (machine_class->default_ram_id && current_machine->ram_size &&
