CTSRD-CHERI/qemu

commit af52257e0980cac13f71d7a7a9b660873a9fa02d

[UPSTREAM?] RISCV: Read the BIOS start address from the ELF file

hw/riscv/boot.c
@@ -36,7 +36,7 @@
 
 void riscv_find_and_load_firmware(MachineState *machine,
                                   const char *default_machine_firmware,
-                                  hwaddr firmware_load_addr)
+                                  hwaddr* firmware_load_addr)
 {
     char *firmware_filename = NULL;
 
@@ -76,7 +76,8 @@ void riscv_find_and_load_firmware(MachineState *machine,
 
     if (firmware_filename) {
         /* If not "none" load the firmware */
-        riscv_load_firmware(firmware_filename, firmware_load_addr);
+        *firmware_load_addr =
+            riscv_load_firmware(firmware_filename, *firmware_load_addr);
         g_free(firmware_filename);
     }
 }

hw/riscv/sifive_u.c
@@ -340,8 +340,7 @@ static void riscv_sifive_u_init(MachineState *machine)
     /* create device tree */
     create_fdt(s, memmap, machine->ram_size, machine->kernel_cmdline);
 
-    riscv_find_and_load_firmware(machine, BIOS_FILENAME,
-                                 memmap[SIFIVE_U_DRAM].base);
+    riscv_find_and_load_firmware(machine, BIOS_FILENAME, &start_addr);
 
     if (machine->kernel_filename) {
         uint64_t kernel_entry = riscv_load_kernel(machine->kernel_filename,

hw/riscv/virt.c
@@ -513,8 +513,7 @@ static void riscv_virt_board_init(MachineState *machine)
     memory_region_add_subregion(system_memory, memmap[VIRT_MROM].base,
                                 mask_rom);
 
-    riscv_find_and_load_firmware(machine, BIOS_FILENAME,
-                                 memmap[VIRT_DRAM].base);
+    riscv_find_and_load_firmware(machine, BIOS_FILENAME, &start_addr);
 
     if (machine->kernel_filename) {
         uint64_t kernel_entry = riscv_load_kernel(machine->kernel_filename,

include/hw/riscv/boot.h
@@ -24,7 +24,7 @@
 
 void riscv_find_and_load_firmware(MachineState *machine,
                                   const char *default_machine_firmware,
-                                  hwaddr firmware_load_addr);
+                                  hwaddr *firmware_load_addr);
 char *riscv_find_firmware(const char *firmware_filename);
 target_ulong riscv_load_firmware(const char *firmware_filename,
                                  hwaddr firmware_load_addr);

commit 894ff8f129f92f13d9f4fb341fd46ce642d2ef45

Fix riscv_find_and_load_firmware for RV32
This was broken in af52257e0980cac13f71d7a7a9b660873a9fa02d

hw/riscv/boot.c
@@ -34,11 +34,12 @@
 # define KERNEL_BOOT_ADDRESS 0x80200000
 #endif
 
-void riscv_find_and_load_firmware(MachineState *machine,
-                                  const char *default_machine_firmware,
-                                  hwaddr* firmware_load_addr)
+target_ulong riscv_find_and_load_firmware(MachineState *machine,
+                                          const char *default_machine_firmware,
+                                          hwaddr firmware_load_addr)
 {
     char *firmware_filename = NULL;
+    target_ulong entry_addr = firmware_load_addr;
 
     if (!machine->firmware) {
         /*
@@ -54,7 +55,7 @@ void riscv_find_and_load_firmware(MachineState *machine,
                         "this happens.");
             warn_report("See QEMU's deprecation documentation for details.");
         }
-        return;
+        return entry_addr;
     }
 
     if (!strcmp(machine->firmware, "default")) {
@@ -76,10 +77,10 @@ void riscv_find_and_load_firmware(MachineState *machine,
 
     if (firmware_filename) {
         /* If not "none" load the firmware */
-        *firmware_load_addr =
-            riscv_load_firmware(firmware_filename, *firmware_load_addr);
+        entry_addr = riscv_load_firmware(firmware_filename, firmware_load_addr);
         g_free(firmware_filename);
     }
+    return entry_addr;
 }
 
 char *riscv_find_firmware(const char *firmware_filename)

hw/riscv/sifive_u.c
@@ -340,7 +340,8 @@ static void riscv_sifive_u_init(MachineState *machine)
     /* create device tree */
     create_fdt(s, memmap, machine->ram_size, machine->kernel_cmdline);
 
-    riscv_find_and_load_firmware(machine, BIOS_FILENAME, &start_addr);
+    start_addr =
+        riscv_find_and_load_firmware(machine, BIOS_FILENAME, start_addr);
 
     if (machine->kernel_filename) {
         uint64_t kernel_entry = riscv_load_kernel(machine->kernel_filename,

hw/riscv/virt.c
@@ -513,7 +513,8 @@ static void riscv_virt_board_init(MachineState *machine)
     memory_region_add_subregion(system_memory, memmap[VIRT_MROM].base,
                                 mask_rom);
 
-    riscv_find_and_load_firmware(machine, BIOS_FILENAME, &start_addr);
+    start_addr =
+        riscv_find_and_load_firmware(machine, BIOS_FILENAME, start_addr);
 
     if (machine->kernel_filename) {
         uint64_t kernel_entry = riscv_load_kernel(machine->kernel_filename,

include/hw/riscv/boot.h
@@ -22,9 +22,9 @@
 
 #include "exec/cpu-defs.h"
 
-void riscv_find_and_load_firmware(MachineState *machine,
-                                  const char *default_machine_firmware,
-                                  hwaddr *firmware_load_addr);
+QEMU_WARN_UNUSED_RESULT target_ulong riscv_find_and_load_firmware(
+    MachineState *machine, const char *default_machine_firmware,
+    hwaddr firmware_load_addr);
 char *riscv_find_firmware(const char *firmware_filename);
 target_ulong riscv_load_firmware(const char *firmware_filename,
                                  hwaddr firmware_load_addr);