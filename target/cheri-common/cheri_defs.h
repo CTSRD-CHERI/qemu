CTSRD-CHERI/qemu

commit 840bde4d1d0cc73df39a653d228acfc46d2b47fe

Fix magic128 build

target/cheri-common/cheri_defs.h
@@ -81,9 +81,14 @@
 #endif
 
 /* compressed capabilities use a 65-bit top, precise and magic 64-bits */
-#if defined(CHERI_128)
+#if defined(CHERI_128) || defined(CHERI_MAGIC128)
+#if defined(CHERI_MAGIC128)
 #define CAP_MAX_LENGTH CC128_MAX_LENGTH
 #define CAP_MAX_TOP CC128_MAX_TOP
+#else
+#define CAP_MAX_LENGTH CC256_NULL_LENGTH
+#define CAP_MAX_TOP CC256_NULL_TOP
+#endif
 #define CAP_MAX_REPRESENTABLE_OTYPE CC128_MAX_REPRESENTABLE_OTYPE
 #define CAP_LAST_NONRESERVED_OTYPE CC128_LAST_NONRESERVED_OTYPE
 #define CAP_OTYPE_UNSEALED CC128_OTYPE_UNSEALED

commit e9c6f747fd3653f9df193eb0fed266556972c1b4

Fix fatal error introduced in 840bde4d1d0cc73df39a653d228acfc46d2b47fe
This resulted in the magic128 MAX_TOP value being used instead of the
real value of 2^64 and caused an assertion when calling
"csetbounds cap, UINT64_MAX". This happens early in the CheriBSD boot so
we couldn't run CheriBSD anymore.

target/cheri-common/cheri_defs.h
@@ -83,11 +83,11 @@
 /* compressed capabilities use a 65-bit top, precise and magic 64-bits */
 #if defined(CHERI_128) || defined(CHERI_MAGIC128)
 #if defined(CHERI_MAGIC128)
-#define CAP_MAX_LENGTH CC128_MAX_LENGTH
-#define CAP_MAX_TOP CC128_MAX_TOP
-#else
 #define CAP_MAX_LENGTH CC256_NULL_LENGTH
 #define CAP_MAX_TOP CC256_NULL_TOP
+#else
+#define CAP_MAX_LENGTH CC128_MAX_LENGTH
+#define CAP_MAX_TOP CC128_MAX_TOP
 #endif
 #define CAP_MAX_REPRESENTABLE_OTYPE CC128_MAX_REPRESENTABLE_OTYPE
 #define CAP_LAST_NONRESERVED_OTYPE CC128_LAST_NONRESERVED_OTYPE