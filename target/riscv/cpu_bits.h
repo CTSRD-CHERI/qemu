b146c2edcb0c0a164ae6a1815d4bafee0552dfd6
Nathaniel Filardo <nfilardo@microsoft.com>
2020-10-22 14:11:46 +0100

    CHERI riscv: cap-load generation support
    This makes [ms]ccsr writable, but only the GCLG bits are updated.
    
    Co-Authored-By: Jessica Clarke <jrtc27@jrtc27.com>

--- target/riscv/cpu_bits.h
+++ target/riscv/cpu_bits.h
@@ -366,6 +366,11 @@
 #define CSR_UCCSR           0x8C0
 #define CSR_SCCSR           0x9C0
 #define CSR_MCCSR           0xBC0
+
+#define XCCSR_ENABLE        0x1 /* Capability extensions enabled */
+#define XCCSR_DIRTY         0x2 /* Capability register written */
+#define SCCSR_SGCLG         0x4 /* Supervisor Global Cap Load Generation */
+#define SCCSR_UGCLG         0x8 /* User Global Cap Load Generation */
 #endif
 
 /* mstatus CSR bits */
