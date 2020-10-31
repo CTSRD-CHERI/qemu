e5ae82ea907eee4bc4394e5ee68ea6b905a5ff3d
Richard Henderson <richard.henderson@linaro.org>
2020-10-30 18:59:09 -0700

    tcg: Add in_code_gen_buffer
    Create a function to determine if a pointer is within the buffer.
    
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    (cherry picked from commit 4846cd37df83b24e65a42bb50e5f407cdb50da72)

--- include/tcg/tcg.h
+++ include/tcg/tcg.h
@@ -696,6 +696,17 @@ extern TCGv _pc_is_current;
 extern TCGv_i32 cpu_rvfi_available_fields;
 #endif
 
+static inline bool in_code_gen_buffer(const void *p)
+{
+    const TCGContext *s = &tcg_init_ctx;
+    /*
+     * Much like it is valid to have a pointer to the byte past the
+     * end of an array (so long as you don't dereference it), allow
+     * a pointer to the byte past the end of the code gen buffer.
+     */
+    return (size_t)(p - s->code_gen_buffer) <= s->code_gen_buffer_size;
+}
+
 static inline size_t temp_idx(TCGTemp *ts)
 {
     ptrdiff_t n = ts - tcg_ctx->temps;
