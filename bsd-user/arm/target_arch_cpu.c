a2fd1a5339998f9cbe38299a97c97c6285df9081
Kyle Evans <kevans@FreeBSD.org>
2020-11-04 13:41:13 -0600

    bsd-user: fix arm-bsd-user with ARM_FEATURE_EL3
    ARM_FEATURE_EL3 was causing rtld to die as the read of TPIDRURO in
    __tls_get_addr (arm/reloc.c) was getting 2 rather than the previous set
    block from the call to sysarch(ARM_SET_TP, ...).
    
    The fix is generally just to make sure that we're using the correct register
    in target_cpu_{get,set}_tls. Notably, if ARM_FEATURE_EL3 is set then we
    should be using the secure variant, env->cp15.tpidruro_s.
    
    I've also taken this opportunity to just match what the kernel does, and
    updates both TPIDRURW and TPIDRURO with the new TLS address.
    
    This will allow us to revert ace334607, which effectively hacked around
    upstream defaulting to ARM_FEATURE_EL3.
    
    Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

--- bsd-user/arm/target_arch_cpu.c
+++ bsd-user/arm/target_arch_cpu.c
@@ -18,10 +18,19 @@
 
 void target_cpu_set_tls(CPUARMState *env, target_ulong newtls)
 {
+    if (access_secure_reg(env)) {
+        env->cp15.tpidrurw_s = newtls;
+        env->cp15.tpidruro_s = newtls;
+        return;
+    }
+
+    env->cp15.tpidr_el[0] = newtls;
     env->cp15.tpidrro_el[0] = newtls;
 }
 
 target_ulong target_cpu_get_tls(CPUARMState *env)
 {
+    if (access_secure_reg(env))
+        return (env->cp15.tpidruro_s);
     return (env->cp15.tpidrro_el[0]);
 }
