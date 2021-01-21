a4f6c51c3862b9fde253cd28703d053ece7a83d1
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2021-01-21 10:31:41 +0000

    Declare existance of the MemOp enum to eliminate warnings.

--- include/exec/helper-head.h
+++ include/exec/helper-head.h
@@ -18,6 +18,8 @@
 #ifndef EXEC_HELPER_HEAD_H
 #define EXEC_HELPER_HEAD_H
 
+typedef enum MemOp MemOp;
+
 #define HELPER(name) glue(helper_, name)
 
 /* Some types that make sense in C, but not for TCG.  */
