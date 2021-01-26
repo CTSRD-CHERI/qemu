dd6bf8a5c9f46ba8b3cd782de75e8f3b9497f956
Lawrence Esswood <le277@cam.ac.uk>
2021-01-26 13:17:57 +0000

    Pay attention to out of bounds exponants for bounds checks

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -150,6 +150,11 @@ static inline bool cap_is_sealed_with_type(const cap_register_t* c) {
 // Check if num_bytes bytes at addr can be read using capability c
 static inline bool cap_is_in_bounds(const cap_register_t* c, uint64_t addr, size_t num_bytes) {
     cheri_debug_assert(num_bytes != 0);
+#ifdef TARGET_AARCH64
+    // Invalid exponant caps are always considered out of bounds.
+    if (!c->cr_bounds_valid)
+        return false;
+#endif
     if (addr < cap_get_base(c)) {
         return false;
     }
