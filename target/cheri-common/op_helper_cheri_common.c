148c0723d2ffc3b5b84789a52e83d30c5bd8f701
Lawrence Esswood <le277@cam.ac.uk>
2021-02-11 18:10:56 +0000

    Add cap load helper that returns un-squashed tag

--- target/cheri-common/op_helper_cheri_common.c
+++ target/cheri-common/op_helper_cheri_common.c
@@ -1247,10 +1247,11 @@ void squash_mutable_permissions(uint64_t *pesbt, const cap_register_t *source)
 #endif
 }
 
-bool load_cap_from_memory_raw(CPUArchState *env, target_ulong *pesbt,
-                              target_ulong *cursor, uint32_t cb,
-                              const cap_register_t *source, target_ulong vaddr,
-                              target_ulong retpc, hwaddr *physaddr)
+bool load_cap_from_memory_raw_tag(CPUArchState *env, target_ulong *pesbt,
+                                  target_ulong *cursor, uint32_t cb,
+                                  const cap_register_t *source,
+                                  target_ulong vaddr, target_ulong retpc,
+                                  hwaddr *physaddr, bool *raw_tag)
 {
     cheri_debug_assert(QEMU_IS_ALIGNED(vaddr, CHERI_CAP_SIZE));
     /*
@@ -1287,6 +1288,8 @@ bool load_cap_from_memory_raw(CPUArchState *env, target_ulong *pesbt,
     }
     int prot;
     bool tag = cheri_tag_get(env, vaddr, cb, physaddr, &prot, retpc);
+    if (raw_tag)
+        *raw_tag = tag;
     if (tag) {
         tag = cheri_tag_prot_clear_or_trap(env, vaddr, cb, source, prot, retpc, tag);
         squash_mutable_permissions(pesbt, source);
@@ -1320,6 +1323,15 @@ bool load_cap_from_memory_raw(CPUArchState *env, target_ulong *pesbt,
     }
 #endif
     return tag;
+}
+
+bool load_cap_from_memory_raw(CPUArchState *env, uint64_t *pesbt,
+                              uint64_t *cursor, uint32_t cb,
+                              const cap_register_t *source, target_ulong vaddr,
+                              target_ulong retpc, hwaddr *physaddr)
+{
+    return load_cap_from_memory_raw_tag(env, pesbt, cursor, cb, source, vaddr,
+                                        retpc, physaddr, NULL);
 }
 
 void load_cap_from_memory(CPUArchState *env, uint32_t cd, uint32_t cb,
