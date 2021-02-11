2df27cd15ec695ba0bf6b30d8baa7c62566d7e6a
Lawrence Esswood <le277@cam.ac.uk>
2021-02-11 18:05:14 +0000

    Add tag set many. Slightly refactor getting / setting multiple tags with atomicity in mind

--- target/cheri-common/cheri_tagmem.h
+++ target/cheri-common/cheri_tagmem.h
@@ -60,6 +60,9 @@ bool cheri_tag_get(CPUArchState *env, target_ulong vaddr, int reg,
                    hwaddr *ret_paddr, int *prot, uintptr_t pc);
 int cheri_tag_get_many(CPUArchState *env, target_ulong vaddr, int reg,
                        hwaddr *ret_paddr, uintptr_t pc);
+void cheri_tag_set_many(CPUArchState *env, uint32_t tags, target_ulong vaddr,
+                        int reg, hwaddr *ret_paddr, uintptr_t pc);
+
 void cheri_tag_set(CPUArchState *env, target_ulong vaddr, int reg,
                    hwaddr *ret_paddr, uintptr_t pc);
 #endif /* TARGET_CHERI */
