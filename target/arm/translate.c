961590d21e44c6c7f7a95933517caa6d7fa925e5
Lawrence Esswood <le277@cam.ac.uk>
2021-03-18 14:38:01 +0000

    Always bounds check with length at least 1

--- target/arm/translate.c
+++ target/arm/translate.c
@@ -9333,6 +9333,7 @@ void restore_state_to_opc(CPUARMState *env, TranslationBlock *tb,
 {
     if (is_a64(env)) {
 #ifdef TARGET_CHERI
+        assert(cap_is_in_bounds(&env->pc.cap, data[0], 4));
         env->pc.cap._cr_cursor = data[0];
 #else
         set_aarch_reg_to_x(&env->pc, data[0]);
