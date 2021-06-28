a3844741b5913ab2ab5ebefa1e0baa6204c365a8
Lawrence Esswood <le277@cam.ac.uk>
2021-06-28 16:02:30 +0100

    Factor out mmu_index from tag set/get

--- accel/tcg/tcg-runtime.h
+++ accel/tcg/tcg-runtime.h
@@ -157,6 +157,8 @@ DEF_HELPER_3(pcc_check_bounds, void, env, tl, tl)
 DEF_HELPER_3(pcc_check_load, cap_checked_ptr, env, tl, memop)
 /* Clear tags due to a store. Only calll this after the store succeeded. */
 DEF_HELPER_3(cheri_invalidate_tags, void, env, cap_checked_ptr, memop)
+/* Clear tags with a different mmu_idx than what would be returned by cpu_mmu_index */
+DEF_HELPER_4(cheri_invalidate_tags_mmu_idx, void, env, cap_checked_ptr, memop, i32)
 /* Clear tags due to a store, the last argument is whether the store succeeded
  */
 DEF_HELPER_4(cheri_invalidate_tags_condition, void, env, cap_checked_ptr, memop,
