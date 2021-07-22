1281cf0d55e26464194924ed5e94f3df09d0752d
Lawrence Esswood <le277@cam.ac.uk>
2021-07-22 13:49:42 +0100

    Check SP alignment for LD|STCT in non C64 mode

--- target/arm/translate-cheri.inc.c
+++ target/arm/translate-cheri.inc.c
@@ -1797,7 +1797,7 @@ TRANS_F(CT)
 
     uint32_t reg = IS_C64(ctx) ? a->Rn : CHERI_EXC_REGNUM_DDC;
 
-    if (reg == 31)
+    if (a->Rn == 31)
         gen_check_sp_alignment(ctx);
 
     TCGv_i64 addr = read_cpu_reg_sp(ctx, a->Rn, 1);
