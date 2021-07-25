624dd1c2cd04054938cc15ba5f4b50e54597505b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-07-25 08:25:17 +0100

    [NFC] Use the capability accessors in a few more places
    I somehow skipped these in my previous search+replace.

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -45,9 +45,9 @@
 #define PRINT_CAP_ARGS_L1(cr)                                                  \
     (cr)->cr_tag, cap_is_sealed_with_type(cr), COMBINED_PERMS_VALUE(cr),       \
         (cr)->cr_flags, cap_get_base(cr), cap_get_length_sat(cr)
-#define PRINT_CAP_FMTSTR_L2 "o:" TARGET_FMT_lx " t:%x"
-#define PRINT_CAP_ARGS_L2(cr) (target_ulong)cap_get_offset(cr), (cr)->cr_otype
-
+#define PRINT_CAP_FMTSTR_L2 "o:" TARGET_FMT_lx " t:" TARGET_FMT_lx
+#define PRINT_CAP_ARGS_L2(cr)                                                  \
+    (target_ulong)cap_get_offset(cr), cap_get_otype_unsigned(cr)
 
 #define PRINT_CAP_FMTSTR PRINT_CAP_FMTSTR_L1 " " PRINT_CAP_FMTSTR_L2
 #define PRINT_CAP_ARGS(cr) PRINT_CAP_ARGS_L1(cr), PRINT_CAP_ARGS_L2(cr)
