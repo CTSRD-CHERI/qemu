3bf6ecf20946011d36a7c5bc9b65a9fb267c7fa2
Lawrence Esswood <le277@cam.ac.uk>
2021-08-20 15:35:47 +0100

    Force morello to be little endian

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -3379,7 +3379,11 @@ static inline bool arm_cpu_data_is_big_endian_a32(CPUARMState *env,
 
 static inline bool arm_cpu_data_is_big_endian_a64(int el, uint64_t sctlr)
 {
+#ifdef TARGET_CHERI
+    return false;
+#else
     return sctlr & (el ? SCTLR_EE : SCTLR_E0E);
+#endif
 }
 
 /* Return true if the processor is in big-endian mode. */
