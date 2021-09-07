69264f018d1f849e3d989f7f27a259c3817e753d
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2021-09-07 12:35:53 +0100

    Define register macros for readability.
    Register numbers are used across the whole RISC-V BSD user-mode.
    The code will be easier to understand if we use macros instead.

--- target/riscv/cpu_user.h
+++ target/riscv/cpu_user.h
@@ -1,19 +1,49 @@
 #ifndef TARGET_RISCV_CPU_USER_H
 #define TARGET_RISCV_CPU_USER_H
 
-#define xRA 1   /* return address (aka link register) */
-#define xSP 2   /* stack pointer */
-#define xGP 3   /* global pointer */
-#define xTP 4   /* thread pointer */
+#define xNULL 0   /* NULL pointer */
+#define xRA   1   /* return address (aka link register) */
+#define xSP   2   /* stack pointer */
+#define xGP   3   /* global pointer */
+#define xTP   4   /* thread pointer */
 
-#define xA0 10  /* gpr[10-17] are syscall arguments */
-#define xA1 11
-#define xA2 12
-#define xA3 13
-#define xA4 14
-#define xA5 15
-#define xA6 16
-#define xA7 17  /* syscall number for RVI ABI */
-#define xT0 5   /* syscall number for RVE ABI */
+/*
+ * Temporary registers.
+ */
+#define xT0   5   /* temporary/alternate link register */
+#define xT1   6
+#define xT2   7
+#define xT3   28
+#define xT4   29
+#define xT5   30
+#define xT6   31
+
+/*
+ * Saved registers.
+ */
+#define xS0   8   /* saved register/frame pointer */
+#define xS1   9
+#define xS2   18
+#define xS3   19
+#define xS4   20
+#define xS5   21
+#define xS6   22
+#define xS7   23
+#define xS8   24
+#define xS9   25
+#define xS10  26
+#define xS11  27
+
+/*
+ * Function arguments.
+ */
+#define xA0   10  /* function argument/return value */
+#define xA1   11  /* function argument/return value */
+#define xA2   12
+#define xA3   13
+#define xA4   14
+#define xA5   15
+#define xA6   16
+#define xA7   17  /* syscall number for RVI ABI */
 
 #endif
