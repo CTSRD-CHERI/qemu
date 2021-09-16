93b3ed4d83bea378ffbbd535d95a2496b3c4098c
Lawrence Esswood <le277@cam.ac.uk>
2021-09-16 16:35:38 +0100

    Improve morello test board to support ACK

--- hw/arm/armtrickbox.c
+++ hw/arm/armtrickbox.c
@@ -28,31 +28,89 @@
  * SUCH DAMAGE.
  */
 
-// The 'trickbox' device used by the test suite for Morello
-// Is an infinitely deep memory mapped fifo that will kill QEMU if \004 is sent
-// or otherwise print to the terminal There may be some other magic numbers this
-// accepts, I have not really reverse engineered the test cases yet.
+// The 'trickbox' device used by the test suite for Morello.
+// This is not an official implementation of any arm technology.
 
-#include "hw/arm/armtrickbox.h"
+// Also includes some of ETMv4
+
 #include "qemu/osdep.h"
 #include "hw/sysbus.h"
+#include "hw/arm/armtrickbox.h"
 #include "qemu/module.h"
 #include "qapi/error.h"
 #include "hw/qdev-properties.h"
 #include "qemu/qemu-print.h"
+#include "map.h"
 
 #define BUF_SIZE 100
+
+
+// List of registers to handle just by getting/setting a field in arm_trickbox_state
+// ITEM(name, reset_value, read_mask, write_mask, write_helper, ... aliases)
+#define REGISTER_LIST(ITEM)                                         \
+    ITEM(Configuration, 0x00004002, 0xFFFFFFFE, 0xFFFFFFFF, NULL)   \
+    ITEM(TargetCPU, 0xF, 0xF, 0xF, NULL)                            \
+    ITEM(abortrgnlo1, 0x40000000, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed, AbortRgn64Lo1) \
+    ITEM(abortrgnhi1, 0x50000000, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed, AbortRgn64Hi1) \
+    ITEM(abortrgnlo2, 0x3F000000, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed, AbortRgn64Lo2) \
+    ITEM(abortrgnhi2, 0x3F080000, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed, AbortRgn64Hi2) \
+    ITEM(AbortRgn64Lo1_Hi, 0x0, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed) \
+    ITEM(AbortRgn64Hi1_Hi, 0x0, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed) \
+    ITEM(AbortRgn64Lo2_Hi, 0x0, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed) \
+    ITEM(AbortRgn64Hi2_Hi, 0x0, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_range_changed) \
+    ITEM(AXIAbortCtl, 0x0, 0xFFFFFFFF, 0xFFFFFFFF, axi_abort_changed) \
+    ITEM(DBGControl, 0x00000181, 0xFFFFFFFF, 0xC1, NULL)
 
 typedef struct {
     SysBusDevice parent_obj;
     MemoryRegion iomem;
+    MemoryRegion iomem_secure;
+    // Tube buffer for more readable output
     char buf[BUF_SIZE + 1];
     size_t ndx;
+    // Registers that are simply backed
+#define DECLARE_BACKING(name, ...) uint32_t name;
+    REGISTER_LIST(DECLARE_BACKING)
 } arm_trickbox_state;
 
 #define TYPE_ARM_TRICKBOX "arm-trickbox"
 #define ARM_TRICKBOX(obj)                                                      \
     OBJECT_CHECK(arm_trickbox_state, (obj), TYPE_ARM_TRICKBOX)
+
+typedef void write_helper_fn(arm_trickbox_state *tb, hwaddr addr, uint32_t *v,
+                             uint32_t old);
+
+static void axi_abort_changed(arm_trickbox_state *tb, hwaddr addr, uint32_t *v,
+                              uint32_t old)
+{
+    if (~*v & (ARM_TRICKBOX_AXIAbortCtl_Rgn1_Rd_Disable |
+               ARM_TRICKBOX_AXIAbortCtl_Rgn1_Wr_Disable |
+               ARM_TRICKBOX_AXIAbortCtl_Rgn2_Rd_Disable |
+               ARM_TRICKBOX_AXIAbortCtl_Rgn2_Wr_Disable)) {
+        printf("TRICKBOX: WARN: Some AXI aborts are enabled but this is not "
+               "yet supported: %x\n",
+               *v);
+    } else {
+        printf("TRICKBOX: INFO: AXI aborts disabled\n");
+    }
+}
+
+static void axi_abort_range_changed(arm_trickbox_state *tb, hwaddr addr,
+                                    uint32_t *v, uint32_t old)
+{
+    // Writes to the 32-bit registers zero extend
+    if (addr == ARM_TRICKBOX_REGISTER_abortrgnlo1)
+        tb->AbortRgn64Lo1_Hi = 0;
+    else if (addr == ARM_TRICKBOX_REGISTER_abortrgnhi1)
+        tb->AbortRgn64Hi1_Hi = 0;
+    else if (addr == ARM_TRICKBOX_REGISTER_abortrgnlo2)
+        tb->AbortRgn64Lo2_Hi = 0;
+    else if (addr == ARM_TRICKBOX_REGISTER_abortrgnhi2)
+        tb->AbortRgn64Hi2_Hi = 0;
+
+    axi_abort_changed(tb, ARM_TRICKBOX_REGISTER_AXIAbortCtl, &tb->AXIAbortCtl,
+                      tb->AXIAbortCtl);
+}
 
 static void empty_buffer(arm_trickbox_state *tb)
 {
@@ -61,21 +119,32 @@ static void empty_buffer(arm_trickbox_state *tb)
     tb->ndx = 0;
 }
 
-static void arm_trickbox_write(void *opaque, hwaddr addr, uint64_t val,
-                               unsigned size)
+const char *const exit_string = NULL; // "FAIL";
+const char *progress = exit_string;
+static void exit_on_string(char c)
 {
-    arm_trickbox_state *tb = ARM_TRICKBOX(opaque);
-
-    assert(size == 1);
+    if (exit_string) {
+        if (*progress != c)
+            progress = exit_string;
+        if (*progress == c)
+            progress++;
+        if (!*progress)
+            assert(0);
+    }
+}
+static void tube_write(arm_trickbox_state *tb, hwaddr addr, uint64_t val,
+                       unsigned size)
+{
     char c = (val & 0xFF);
 
+    exit_on_string(c);
+
     switch (c) {
-    case ARM_TRICKBOX_EXIT_C:
+    case ARM_TRICKBOX_TUBE_EXIT:
         if (tb->ndx != 0)
             empty_buffer(tb);
         exit(0);
     default:
-        // Is there are QEMU specific way to do this?
         tb->buf[tb->ndx++] = c;
     }
 
@@ -83,12 +152,75 @@ static void arm_trickbox_write(void *opaque, hwaddr addr, uint64_t val,
         empty_buffer(tb);
 }
 
-static uint64_t arm_trickbox_read(void *opaque, hwaddr addr, unsigned size)
+static void arm_trickbox_write(void *opaque, hwaddr addr, uint64_t val,
+                               unsigned size)
 {
-    return 0;
+    arm_trickbox_state *tb = ARM_TRICKBOX(opaque);
+
+    // Most registers are aligned to 4 bytes
+    hwaddr register_addr = (addr) & ~(4 - 1);
+
+    // printf("T-write %lx\n", register_addr);
+
+    uint32_t old;
+
+    switch (register_addr) {
+
+#define CASE_HANDLE(name) case ARM_TRICKBOX_REGISTER_##name:
+#define HANDLE_WRITE(name, resetv, readmask, writemask, writefn, ...)          \
+    MAP(CASE_HANDLE, name, ##__VA_ARGS__)                                      \
+    old = tb->name;                                                            \
+    tb->name = (tb->name & ~writemask) | (val & writemask);                    \
+    if (writefn) {                                                             \
+        ((write_helper_fn *)writefn)(tb, addr, &tb->name, old);                \
+    }                                                                          \
+    break;
+        REGISTER_LIST(HANDLE_WRITE)
+
+    case ARM_TRICKBOX_REGISTER_Tube:
+        tube_write(tb, addr, val, size);
+        break;
+    default:
+        // TODO: Once most functionality is implemented, make this a log.
+        printf("Write at %lx\n", register_addr);
+        assert(0);
+    }
 }
 
-static void arm_trickbox_reset(DeviceState *dev) {}
+static uint64_t arm_trickbox_read(void *opaque, hwaddr addr, unsigned size)
+{
+    arm_trickbox_state *tb = ARM_TRICKBOX(opaque);
+
+    // Most registers are aligned to 4 bytes
+    hwaddr register_addr = (addr) & ~(4 - 1);
+
+    // printf("T-read %lx\n", register_addr);
+
+    switch (register_addr) {
+
+#define HANDLE_READ(name, resetv, readmask, writemask, writefn, ...)           \
+    MAP(CASE_HANDLE, name, ##__VA_ARGS__)                                      \
+    return tb->name & readmask;
+        REGISTER_LIST(HANDLE_READ)
+
+    case ARM_TRICKBOX_REGISTER_Tube:
+        return 0;
+    default:
+        // TODO: Once most functionality is implemented, make this a log.
+        assert(0);
+    }
+}
+
+static void arm_trickbox_reset(DeviceState *dev)
+{
+    arm_trickbox_state *tb = ARM_TRICKBOX(dev);
+
+#define RESET_REG(name, resetv, ...) tb->name = resetv;
+    REGISTER_LIST(RESET_REG)
+
+    axi_abort_changed(tb, ARM_TRICKBOX_REGISTER_AXIAbortCtl, &tb->AXIAbortCtl,
+                      tb->AXIAbortCtl);
+}
 
 static const MemoryRegionOps arm_trickbox_ops = {
     .read = arm_trickbox_read,
@@ -100,8 +232,12 @@ static void arm_trickbox_instance_init(Object *obj)
 {
     SysBusDevice *dev = SYS_BUS_DEVICE(obj);
     arm_trickbox_state *s = ARM_TRICKBOX(dev);
-    memory_region_init_io(&s->iomem, obj, &arm_trickbox_ops, s, "trickbox", 1);
+    memory_region_init_io(&s->iomem, obj, &arm_trickbox_ops, s, "trickbox",
+                          ARM_TRICKBOX_SIZE);
+    memory_region_init_io(&s->iomem_secure, obj, &arm_trickbox_ops, s,
+                          "trickbox_sec", ARM_TRICKBOX_SIZE);
     sysbus_init_mmio(dev, &s->iomem);
+    sysbus_init_mmio(dev, &s->iomem_secure);
 }
 
 static void arm_trickbox_realize(DeviceState *dev, Error **errp) {}
@@ -135,15 +271,16 @@ static void arm_trickbox_register(void)
 
 type_init(arm_trickbox_register)
 
-    static void arm_trickbox_mm_init(hwaddr base)
+    static SysBusDevice *arm_trickbox_mm_init(hwaddr base)
 {
     DeviceState *dev;
     dev = qdev_new(TYPE_ARM_TRICKBOX);
     sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
     sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, base);
+    return SYS_BUS_DEVICE(dev);
 }
 
-void arm_trickbox_mm_init_default(void)
+SysBusDevice *arm_trickbox_mm_init_default(void)
 {
-    arm_trickbox_mm_init(ARM_TRICKBOX_ADDRESS);
+    return arm_trickbox_mm_init(ARM_TRICKBOX_ADDRESS);
 }
--- hw/arm/morello.c
+++ hw/arm/morello.c
@@ -41,6 +41,14 @@
 #include "sysemu/sysemu.h"
 #include "hw/arm/armtrickbox.h"
 
+/* A basic morello board. There is no need to use this if only a morello CPU
+ * is required (use the virt board instead). This board is designed to be a
+ * minimal platform for testing, with optional support for the ARM ACK.
+ */
+
+// TODO: This should be passed in on the command line
+#define CONFIGURE_ACK false
+
 #define RAM_SIZE 0x13000000
 
 #define MORELLO_MAX_CPUS 4
@@ -79,6 +87,46 @@ static void error_and_die(const char *message)
 // Lifetime is for the lifetime of the program
 static struct arm_boot_info binfo;
 
+// ACK test ELSs do not define an entry point. Instead, look for this symbol.
+uint64_t RVBAR_exec;
+static void find_rvbar_exec(const char *st_name, int st_info, uint64_t st_value,
+                            uint64_t st_size)
+{
+    if (strcmp(st_name, "__RVBAR_ENTRY") == 0) {
+        RVBAR_exec = st_value;
+        printf("%lx\n", RVBAR_exec);
+    }
+}
+
+struct mem_region {
+    uint64_t start, end;
+    bool alias_sec;
+    const char *name;
+    const char *name_sec;
+};
+
+static MemoryRegion *create_mem_from_desc(MemoryRegion *parent,
+                                          struct mem_region *el, bool sec,
+                                          MemoryRegion *alias)
+{
+    MemoryRegion *mem = g_new(MemoryRegion, 1);
+
+    if (alias) {
+        memory_region_init_alias(mem, NULL, sec ? el->name_sec : el->name,
+                                 alias, 0, el->end - el->start);
+    } else {
+        memory_region_init_ram(mem, NULL, sec ? el->name_sec : el->name,
+                               el->end - el->start, &error_fatal);
+#ifdef TARGET_CHERI
+        cheri_tag_init(mem, el->end - el->start);
+#endif
+    }
+
+    memory_region_add_subregion(parent, el->start, mem);
+
+    return mem;
+}
+
 static void morello_machine_init(MachineState *machine)
 {
     MorelloMachineState *morelloMachineState = MORELLO_MACHINE(machine);
@@ -88,13 +136,40 @@ static void morello_machine_init(MachineState *machine)
 #error "Morello board should be used with TARGET_CHERI"
 #endif
 
-    /* Add RAM */
-    memory_region_add_subregion(get_system_memory(), 0, machine->ram);
+    // Set to true to get seperate physical address spaces for regions specified
+    // below with secure aliases with non secure = false.
+    bool seperate_s_ns = false;
 
-#ifdef TARGET_CHERI
-    /* Then tag it all */
-    cheri_tag_init(machine->ram, machine->ram_size);
-#endif
+    // This map was decided on just by running the tests. It would be good
+    // to have a better idea of what the actual memory map is.
+#define NAME(N) N, N "_sec"
+    struct mem_region mem_map[] = {
+        // {start, end, secure aliases with non secure, name}
+        {0x0, 0x13000000, true, NAME("LOW")},
+        {0x13200000, 0x100000000, true, NAME("HIGH")},
+        //{0x50200000, 0x50300000, true, NAME("SEPERATE")},
+        //{0x50200000, 0x100000000, true, NAME("HIGHER")},
+        {0x800000000000, 0x800010000000, true, NAME("HIGHEST")},
+        {0xfffffffe0000, 0x1000000000000, true, NAME("mem.NORMAL_MAX_PA")},
+    };
+
+    MemoryRegion *secure_memory = NULL;
+    if (seperate_s_ns) {
+        secure_memory = g_new(MemoryRegion, 1);
+        memory_region_init(secure_memory, NULL, "cpu-secure-memory",
+                           UINT64_MAX);
+    }
+
+    /* Add RAM */
+    for (struct mem_region *el = mem_map;
+         el != (struct mem_region *)((char *)mem_map + sizeof(mem_map)); el++) {
+        MemoryRegion *ns =
+            create_mem_from_desc(get_system_memory(), el, false, NULL);
+        if (seperate_s_ns) {
+            create_mem_from_desc(secure_memory, el, true,
+                                 el->alias_sec ? ns : NULL);
+        }
+    }
 
     if (strcmp(machine->cpu_type, ARM_CPU_TYPE_NAME("morello")) != 0) {
         error_and_die(
@@ -107,6 +182,7 @@ static void morello_machine_init(MachineState *machine)
         object_initialize_child(OBJECT(machine), "cpu[*]",
                                 &morelloMachineState->cpus[i].core,
                                 machine->cpu_type);
+        morelloMachineState->cpus[i].core.secure_memory = secure_memory;
         if (!qdev_realize(DEVICE(&morelloMachineState->cpus[i].core), NULL,
                           &errp)) {
             error_report_err(errp);
@@ -115,18 +191,34 @@ static void morello_machine_init(MachineState *machine)
     }
 
     /* Create the trickbox used for testing */
-    arm_trickbox_mm_init_default();
+    SysBusDevice *trickbox = arm_trickbox_mm_init_default();
+
+    if (seperate_s_ns) {
+        // Put the trickbox in the secure space as well
+        trickbox->mmio[1].addr = trickbox->mmio[0].addr;
+        memory_region_add_subregion(secure_memory, trickbox->mmio[1].addr,
+                                    trickbox->mmio[1].memory);
+    }
 
     /* Start logging */
 #ifdef CONFIG_TCG_LOG_INSTR
     qemu_log_instr_init(CPU(ARM_CPU(first_cpu)));
 #endif
 
+    // This RAM size is now wrong given the ack map above
     binfo.ram_size = ram_size;
     binfo.nb_cpus = machine->smp.cpus;
+    binfo.secure_boot = seperate_s_ns;
+
+    if (CONFIGURE_ACK)
+        binfo.sym_cb = &find_rvbar_exec;
+
     arm_load_kernel(ARM_CPU(first_cpu), machine, &binfo);
 
-    assert(binfo.entry < ram_size);
+    if (CONFIGURE_ACK) {
+        assert(RVBAR_exec != 0);
+        binfo.entry = RVBAR_exec;
+    }
 }
 
 static void morello_machine_class_init(ObjectClass *oc, void *data)
@@ -137,8 +229,6 @@ static void morello_machine_class_init(ObjectClass *oc, void *data)
     mc->init = morello_machine_init;
     mc->default_cpus = mc->min_cpus = 1;
     mc->max_cpus = MORELLO_MAX_CPUS;
-    mc->default_ram_size = RAM_SIZE;
-    mc->default_ram_id = "ram";
     mc->default_cpu_type = ARM_CPU_TYPE_NAME("morello");
     mc->minimum_page_bits = 12;
 }
