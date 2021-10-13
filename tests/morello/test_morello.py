0bac8e9c6f6d6dd58ff229ec872b4a2b3817d418
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-10-13 15:27:07 +0100

    Try to make the Morello tests compatible with python 3.6

--- tests/morello/test_morello.py
+++ tests/morello/test_morello.py
@@ -86,7 +86,8 @@ def find_morello_tests():
 def test_morello_elf_file(elf_file, should_print_failed: bool, qemu_binary):
     command = [str(qemu_binary), *qemu_args, elf_file]
     # Timeout will fail the test
-    sp = subprocess.run(command, capture_output=True, timeout=5)
+    sp = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                        timeout=5)
     code = sp.returncode
     result = sp.stdout
     result_str = result.decode("utf-8")
