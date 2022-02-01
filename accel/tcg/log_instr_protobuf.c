d73bc35b6940e60161640b5b56f7ecb34c0f9efd
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2022-02-01 21:53:28 +0000

    Use perfetto protobuf definition for the protobuf backend.

--- accel/tcg/log_instr_protobuf.c
+++ accel/tcg/log_instr_protobuf.c
@@ -44,16 +44,56 @@
 #include "exec/memop.h"
 #include "disas/disas.h"
 
-#include "protobuf_backend_entry.pb-c.h"
+#include "qemu_log_entry.pb-c.h"
 
 static FILE *protobuf_logfile;
 
-#ifdef TARGET_CHERI
-static QEMULogInstrCapability *emit_protobuf_capability(cap_register_t *cap)
-{
-    QEMULogInstrCapability *pb_cap = g_malloc(sizeof(QEMULogInstrCapability));
+/*
+ * Helpers to simplify access to namespaced protobuf values
+ * For consistency, redefine types to mirror the generated
+ * code from protobuf-c but strip the package prefix.
+ */
+#define GEN_TYPE_NS(x)     Perfetto__Protos__##x
+#define GEN_TYPE_PREFIX(x) GEN_TYPE_NS(QEMULogEntry##x)
+typedef GEN_TYPE_PREFIX(Capability) QEMULogEntryCapability;
+typedef GEN_TYPE_PREFIX(Mem) QEMULogEntryMem;
+typedef GEN_TYPE_PREFIX(Reg) QEMULogEntryReg;
+typedef GEN_TYPE_PREFIX(Evt) QEMULogEntryEvt;
+typedef GEN_TYPE_PREFIX(Exc) QEMULogEntryExc;
+typedef GEN_TYPE_PREFIX() QEMULogEntry;
+typedef GEN_TYPE_PREFIX(EvtTraceState) QEMULogEntryEvtTraceState;
+typedef GEN_TYPE_PREFIX(EvtCtxUpdate) QEMULogEntryEvtCtxUpdate;
+typedef GEN_TYPE_NS(QEMULogEventTraceState) QEMULogEventTraceState;
 
-    qemulog_instr_capability__init(pb_cap);
+#define GEN_FN_NS(x)                        perfetto__protos__##x
+#define GEN_FN_PREFIX(x)                    GEN_FN_NS(qemulog_entry_##x)
+#define qemulog_entry_capability__init      GEN_FN_PREFIX(capability__init)
+#define qemulog_entry_mem__init             GEN_FN_PREFIX(mem__init)
+#define qemulog_entry_reg__init             GEN_FN_PREFIX(reg__init)
+#define qemulog_entry_evt__init             GEN_FN_PREFIX(evt__init)
+#define qemulog_entry_evt_trace_state__init GEN_FN_PREFIX(evt_trace_state__init)
+#define qemulog_entry_evt_ctx_update__init  GEN_FN_PREFIX(evt_ctx_update__init)
+#define qemulog_entry_evt_regdump__init     GEN_FN_PREFIX(evt_regdump__init)
+#define qemulog_entry__get_packed_size      GEN_FN_PREFIX(_get_packed_size)
+#define qemulog_entry__pack                 GEN_FN_PREFIX(_pack)
+
+#define GEN_CONST_NS(x)         PERFETTO__PROTOS__##x
+#define GEN_CONST_PREFIX(x)     GEN_CONST_NS(QEMULOG_ENTRY_##x)
+#define ENUM_ENTRY_MEM_VALUE(v) GEN_CONST_PREFIX(MEM__VALUE_##v)
+#define ENUM_ENTRY_REG_VALUE(v) GEN_CONST_PREFIX(REG__VALUE_##v)
+#define ENUM_ENTRY_EVT_CASE(v)  GEN_CONST_PREFIX(EVT__EVENT_##v)
+#define ENUM_ENTRY_INSN_CASE(v) GEN_CONST_PREFIX(_INSN_##v)
+#define ENUM_ENTRY_MODE_CASE(v) GEN_CONST_PREFIX(_MODE_MODE_##v)
+#define ENUM_ENTRY_EXC_CASE(v)  GEN_CONST_PREFIX(EXC_TYPE__##v)
+#define QEMULOG_ENTRY__INIT     GEN_CONST_NS(QEMULOG_ENTRY__INIT)
+#define QEMULOG_ENTRY_EXC__INIT GEN_CONST_PREFIX(EXC__INIT)
+
+#ifdef TARGET_CHERI
+static QEMULogEntryCapability *emit_protobuf_capability(cap_register_t *cap)
+{
+    QEMULogEntryCapability *pb_cap = g_malloc(sizeof(QEMULogEntryCapability));
+
+    qemulog_entry_capability__init(pb_cap);
     pb_cap->valid = cap->cr_tag;
     pb_cap->sealed = cap_is_sealed_with_type(cap);
     pb_cap->perms = COMBINED_PERMS_VALUE(cap);
@@ -67,11 +107,11 @@ static QEMULogInstrCapability *emit_protobuf_capability(cap_register_t *cap)
 #endif
 
 static void emit_protobuf_meminfo(log_meminfo_t *minfo,
-                                  QEMULogInstrMem **pb_mem)
+                                  QEMULogEntryMem **pb_mem)
 {
-    QEMULogInstrMem *mem = g_malloc(sizeof(QEMULogInstrMem));
+    QEMULogEntryMem *mem = g_malloc(sizeof(QEMULogEntryMem));
 
-    qemulog_instr_mem__init(mem);
+    qemulog_entry_mem__init(mem);
     mem->is_load = (minfo->flags & LMI_LD) != 0;
     mem->size = memop_size(minfo->op);
     mem->addr = minfo->addr;
@@ -80,31 +120,31 @@ static void emit_protobuf_meminfo(log_meminfo_t *minfo,
            "Capability memory access without CHERI support");
 #else
     if (minfo->flags & LMI_CAP) {
-        mem->value_case = QEMULOG_INSTR_MEM__VALUE_CAP_VALUE;
+        mem->value_case = ENUM_ENTRY_MEM_VALUE(CAP_VALUE);
         mem->cap_value = emit_protobuf_capability(&minfo->cap);
     } else
 #endif
     {
-        mem->value_case = QEMULOG_INSTR_MEM__VALUE_INT_VALUE;
+        mem->value_case = ENUM_ENTRY_MEM_VALUE(INT_VALUE);
         mem->int_value = minfo->value;
     }
     *pb_mem = mem;
 }
 
-static void release_protobuf_meminfo(QEMULogInstrMem *pb_mem)
+static void release_protobuf_meminfo(QEMULogEntryMem *pb_mem)
 {
-    if (pb_mem->value_case == QEMULOG_INSTR_MEM__VALUE_CAP_VALUE) {
+    if (pb_mem->value_case == ENUM_ENTRY_MEM_VALUE(CAP_VALUE)) {
         g_free(pb_mem->cap_value);
     }
     g_free(pb_mem);
 }
 
 static void emit_protobuf_reginfo(log_reginfo_t *rinfo,
-                                  QEMULogInstrReg **pb_reg)
+                                  QEMULogEntryReg **pb_reg)
 {
-    QEMULogInstrReg *reg = g_malloc(sizeof(QEMULogInstrReg));
+    QEMULogEntryReg *reg = g_malloc(sizeof(QEMULogEntryReg));
 
-    qemulog_instr_reg__init(reg);
+    qemulog_entry_reg__init(reg);
     /* Safe to de-const cast as the pointer is only used during packing */
     reg->name = (char *)rinfo->name;
 
@@ -114,46 +154,47 @@ static void emit_protobuf_reginfo(log_reginfo_t *rinfo,
 #else
     if (reginfo_is_cap(rinfo)) {
         if (reginfo_has_cap(rinfo)) {
-            reg->value_case = QEMULOG_INSTR_REG__VALUE_CAP_VALUE;
+            reg->value_case = ENUM_ENTRY_REG_VALUE(CAP_VALUE);
             reg->cap_value = emit_protobuf_capability(&rinfo->cap);
         } else {
-            reg->value_case = QEMULOG_INSTR_REG__VALUE_INT_VALUE;
+            reg->value_case = ENUM_ENTRY_REG_VALUE(INT_VALUE);
             reg->int_value = rinfo->gpr;
         }
     } else
 #endif
     {
-        reg->value_case = QEMULOG_INSTR_REG__VALUE_INT_VALUE;
+        reg->value_case = ENUM_ENTRY_REG_VALUE(INT_VALUE);
         reg->int_value = rinfo->gpr;
     }
     *pb_reg = reg;
 }
 
-static void release_protobuf_reginfo(QEMULogInstrReg *pb_reg)
+static void release_protobuf_reginfo(QEMULogEntryReg *pb_reg)
 {
-    if (pb_reg->value_case == QEMULOG_INSTR_REG__VALUE_CAP_VALUE) {
+    if (pb_reg->value_case == ENUM_ENTRY_REG_VALUE(CAP_VALUE)) {
         g_free(pb_reg->cap_value);
     }
     g_free(pb_reg);
 }
 
-static void emit_protobuf_event(log_event_t *evtinfo, QEMULogInstrEvt **pb_evt)
+static void emit_protobuf_event(log_event_t *evtinfo, QEMULogEntryEvt **pb_evt)
 {
-    QEMULogInstrEvt *evt = g_malloc(sizeof(QEMULogInstrEvt));
+    QEMULogEntryEvt *evt = g_malloc(sizeof(QEMULogEntryEvt));
 
-    qemulog_instr_evt__init(evt);
+    qemulog_entry_evt__init(evt);
     switch (evtinfo->id) {
     case LOG_EVENT_STATE:
-        evt->event_case = QEMULOG_INSTR_EVT__EVENT_STATE;
-        evt->state = g_malloc(sizeof(QEMULogInstrEvtTraceState));
-        qemulog_instr_evt_trace_state__init(evt->state);
-        evt->state->next_state = (LogEventTraceState)evtinfo->state.next_state;
+        evt->event_case = ENUM_ENTRY_EVT_CASE(STATE);
+        evt->state = g_malloc(sizeof(QEMULogEntryEvtTraceState));
+        qemulog_entry_evt_trace_state__init(evt->state);
+        evt->state->next_state =
+            (QEMULogEventTraceState)evtinfo->state.next_state;
         evt->state->pc = evtinfo->state.pc;
         break;
     case LOG_EVENT_CTX_UPDATE:
-        evt->event_case = QEMULOG_INSTR_EVT__EVENT_CTX_UPDATE;
-        evt->ctx_update = g_malloc(sizeof(QEMULogInstrEvtCtxUpdate));
-        qemulog_instr_evt_ctx_update__init(evt->ctx_update);
+        evt->event_case = ENUM_ENTRY_EVT_CASE(CTX_UPDATE);
+        evt->ctx_update = g_malloc(sizeof(QEMULogEntryEvtCtxUpdate));
+        qemulog_entry_evt_ctx_update__init(evt->ctx_update);
         evt->ctx_update->pid = evtinfo->ctx_update.pid;
         evt->ctx_update->tid = evtinfo->ctx_update.tid;
         evt->ctx_update->cid = evtinfo->ctx_update.cid;
@@ -162,7 +203,7 @@ static void emit_protobuf_event(log_event_t *evtinfo, QEMULogInstrEvt **pb_evt)
             (char *)cpu_get_mode_name(evtinfo->ctx_update.mode);
         break;
     case LOG_EVENT_MARKER:
-        evt->event_case = QEMULOG_INSTR_EVT__EVENT_MARKER;
+        evt->event_case = ENUM_ENTRY_EVT_CASE(MARKER);
         evt->marker = evtinfo->marker;
         break;
     default:
@@ -171,14 +212,17 @@ static void emit_protobuf_event(log_event_t *evtinfo, QEMULogInstrEvt **pb_evt)
     *pb_evt = evt;
 }
 
-static void release_protobuf_event(QEMULogInstrEvt *pb_evt)
+static void release_protobuf_event(QEMULogEntryEvt *pb_evt)
 {
-    if (pb_evt->event_case == QEMULOG_INSTR_EVT__EVENT_STATE) {
+    if (pb_evt->event_case == ENUM_ENTRY_EVT_CASE(STATE)) {
         g_free(pb_evt->state);
     }
-    if (pb_evt->event_case == QEMULOG_INSTR_EVT__EVENT_CTX_UPDATE) {
+    if (pb_evt->event_case == ENUM_ENTRY_EVT_CASE(CTX_UPDATE)) {
         g_free(pb_evt->ctx_update);
     }
+    /* if (pb_evt->event_case == ENUM_ENTRY_EVT_CASE(REGDUMP)) { */
+    /*     g_free(pb_evt->regdump); */
+    /* } */
     g_free(pb_evt);
 }
 
@@ -207,11 +251,11 @@ void sync_protobuf_backend(CPUArchState *env)
 
 void emit_protobuf_entry(CPUArchState *env, cpu_log_entry_t *entry)
 {
-    QEMULogInstrEntry pb_entry = QEMULOG_INSTR_ENTRY__INIT;
-    QEMULogInstrExc pb_exc = QEMULOG_INSTR_EXC__INIT;
-    QEMULogInstrMem **pb_mem;
-    QEMULogInstrReg **pb_reg;
-    QEMULogInstrEvt **pb_evt;
+    QEMULogEntry pb_entry = QEMULOG_ENTRY__INIT;
+    QEMULogEntryExc pb_exc = QEMULOG_ENTRY_EXC__INIT;
+    QEMULogEntryMem **pb_mem;
+    QEMULogEntryReg **pb_reg;
+    QEMULogEntryEvt **pb_evt;
     uint8_t *buf;
     size_t len;
     uint32_t preamble = 0;
@@ -220,7 +264,7 @@ void emit_protobuf_entry(CPUArchState *env, cpu_log_entry_t *entry)
     pb_entry.pc = entry->pc;
 
     if (entry->flags & LI_FLAG_HAS_INSTR_DATA) {
-        pb_entry.insn_case = QEMULOG_INSTR_ENTRY__INSN_DISAS;
+        pb_entry.insn_case = ENUM_ENTRY_INSN_CASE(DISAS);
         pb_entry.disas = disas_one_strbuf(env_cpu(env), entry->insn_bytes,
                                           sizeof(entry->insn_bytes), entry->pc);
         pb_entry.cpu = env_cpu(env)->cpu_index;
@@ -229,23 +273,25 @@ void emit_protobuf_entry(CPUArchState *env, cpu_log_entry_t *entry)
             /*
              * Safe to de-const cast as the pointer is only used during packing
              */
-            pb_entry.mode = (char *)cpu_get_mode_name(entry->next_cpu_mode);
+            pb_entry.mode_case = ENUM_ENTRY_MODE_CASE(NAME);
+            pb_entry.mode_name =
+                (char *)cpu_get_mode_name(entry->next_cpu_mode);
         }
         if (entry->flags & LI_FLAG_INTR_MASK) {
             pb_entry.exception = &pb_exc;
             pb_exc.vector = entry->intr_vector;
             pb_exc.code = entry->intr_code;
             if (entry->flags & LI_FLAG_INTR_TRAP) {
-                pb_exc.type = QEMULOG_INSTR_EXC_TYPE__TRAP;
+                pb_exc.type = ENUM_ENTRY_EXC_CASE(TRAP);
                 pb_exc.faultaddr = entry->intr_faultaddr;
             } else if (entry->flags & LI_FLAG_INTR_ASYNC) {
-                pb_exc.type = QEMULOG_INSTR_EXC_TYPE__INTR;
+                pb_exc.type = ENUM_ENTRY_EXC_CASE(INTR);
             } else {
                 assert(0 && "invalid exception flags");
             }
         }
         if (entry->mem->len > 0) {
-            pb_mem = g_malloc(entry->mem->len * sizeof(QEMULogInstrMem *));
+            pb_mem = g_malloc(entry->mem->len * sizeof(QEMULogEntryMem *));
             pb_entry.n_mem = entry->mem->len;
             pb_entry.mem = pb_mem;
             for (i = 0; i < entry->mem->len; i++, pb_mem++) {
@@ -255,7 +301,7 @@ void emit_protobuf_entry(CPUArchState *env, cpu_log_entry_t *entry)
             }
         }
         if (entry->regs->len > 0) {
-            pb_reg = g_malloc(entry->regs->len * sizeof(QEMULogInstrReg *));
+            pb_reg = g_malloc(entry->regs->len * sizeof(QEMULogEntryReg *));
             pb_entry.n_reg = entry->regs->len;
             pb_entry.reg = pb_reg;
             for (i = 0; i < entry->regs->len; i++, pb_reg++) {
@@ -268,7 +314,7 @@ void emit_protobuf_entry(CPUArchState *env, cpu_log_entry_t *entry)
 
     if (entry->events->len > 0) {
         pb_evt = NULL;
-        pb_evt = g_malloc(entry->events->len * sizeof(QEMULogInstrEvt *));
+        pb_evt = g_malloc(entry->events->len * sizeof(QEMULogEntryEvt *));
         pb_entry.n_evt = entry->events->len;
         pb_entry.evt = pb_evt;
         for (i = 0; i < entry->events->len; i++, pb_evt++) {
@@ -277,13 +323,13 @@ void emit_protobuf_entry(CPUArchState *env, cpu_log_entry_t *entry)
         }
     }
 
-    len = qemulog_instr_entry__get_packed_size(&pb_entry);
+    len = qemulog_entry__get_packed_size(&pb_entry);
     preamble = len;
     assert(preamble == len && "Message preamble overflow");
 
     buf = g_malloc(len + sizeof(preamble));
     *((uint32_t *)buf) = preamble;
-    qemulog_instr_entry__pack(&pb_entry, buf + sizeof(preamble));
+    qemulog_entry__pack(&pb_entry, buf + sizeof(preamble));
 
     /*
      * Technically we should not use the qemu logfile as it is not open in
