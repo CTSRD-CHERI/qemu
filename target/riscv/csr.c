6a28df34b8b5f20a71aca372904fd53e3217c378
Atish Patra <atishp@rivosinc.com>
2022-03-03 10:54:38 -0800

    target/riscv: Add support for mconfigptr
    commit 3e6a417c8a077595ebcb4fb1d0944b291564cd43 upstream
    
    RISC-V privileged specification v1.12 introduced a mconfigptr
    which will hold the physical address of a configuration data
    structure. As Qemu doesn't have a configuration data structure,
    is read as zero which is valid as per the priv spec.
    
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Message-Id: <20220303185440.512391-5-atishp@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    [ Changed CSR function table initialisations to use macros ]
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -3950,6 +3950,8 @@ riscv_csr_operations csr_ops[CSR_TABLE_SIZE] = {
     [CSR_MIMPID] =              CSR_OP_FN_R(any, read_zero, "mimppid"),
     [CSR_MHARTID] =             CSR_OP_R(any, mhartid),
 
+    [CSR_MCONFIGPTR]  = { "mconfigptr", any,   read_zero,
+                                        .min_priv_ver = PRIV_VERSION_1_12_0 },
     /* Machine Trap Setup */
     [CSR_MSTATUS]     = { "mstatus",    any,   read_mstatus,     write_mstatus, NULL,
                                                read_mstatus_i128                   },
