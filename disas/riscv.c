514635ea947fd44122fa9130782b5fc63873d2f6
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-04-27 09:27:08 -0700

    [CHERI-RISC-V] Add support for CGetHigh

--- disas/riscv.c
+++ disas/riscv.c
@@ -514,6 +514,7 @@ typedef enum {
     rv_op_cmove,
     rv_op_ccleartag,
     rv_op_cjalr,
+    rv_op_cgethigh,
     rv_op_cgetaddr,
     rv_op_csealentry,
     rv_op_cloadtags,
@@ -1232,6 +1233,7 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_ccleartag] = { "ccleartag", rv_codec_r, rv_fmt_cd_cs1, NULL, 0, 0, 0 },
     [rv_op_cjalr] = { "cjalr", rv_codec_r, rv_fmt_cd_cs1, NULL, 0, 0, 0 },
     [rv_op_cgetaddr] = { "cgetaddr", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
+    [rv_op_cgethigh] = { "cgetaddr", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
     [rv_op_csealentry] = { "csealentry", rv_codec_r, rv_fmt_cd_cs1, NULL, 0, 0, 0 },
     [rv_op_cloadtags] = { "cloadtags", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
 
@@ -1503,6 +1505,7 @@ static rv_opcode decode_cheri_two_op(unsigned func) {
     case 0b01111: return rv_op_cgetaddr;
     case 0b10001: return rv_op_csealentry;
     case 0b10010: return rv_op_cloadtags;
+    case 0b10111: return rv_op_cgethigh;
     default: return rv_op_illegal;
     }
 }
--- target/cheri-common/op_helper_cheri_common.c
+++ target/cheri-common/op_helper_cheri_common.c
@@ -258,6 +258,15 @@ target_ulong CHERI_HELPER_IMPL(cgetflags(CPUArchState *env, uint32_t cb))
     return (target_ulong)cap_get_flags(get_readonly_capreg(env, cb));
 }
 
+target_ulong CHERI_HELPER_IMPL(cgethigh(CPUArchState *env, uint32_t cb))
+{
+    /*
+     * CGetHigh: Move Metadata bits to a General-Purpose Register
+     * TODO: could do this directly from TCG now.
+     */
+    return CAP_cc(compress_mem)(get_readonly_capreg(env, cb));
+}
+
 target_ulong CHERI_HELPER_IMPL(cgetlen(CPUArchState *env, uint32_t cb))
 {
     /*
