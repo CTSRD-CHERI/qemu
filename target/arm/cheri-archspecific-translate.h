cbf2968eceb6682fec8360ffeb302309bd09c53a
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-02-28 11:54:04 -0800

    Fix -Wunused-function for non-Morello builds

--- target/arm/cheri-archspecific-translate.h
+++ target/arm/cheri-archspecific-translate.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#ifdef TARGET_CHERI
 static inline bool cctlr_set(struct DisasContext *ctx, uint32_t bits);
 #define CHERI_TRANSLATE_DDC_RELOCATION(ctx) cctlr_set(ctx, CCTLR_DDCBO)
 #define CHERI_TRANSLATE_PCC_RELOCATION(ctx) cctlr_set(ctx, CCTLR_PCCBO)
+#endif
