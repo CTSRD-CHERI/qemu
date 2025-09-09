ce7cf2a5201fc753c21cf0a01e38d4a38659819c
Yuecheng Wang <yw737@cl.cam.ac.uk>
2025-09-09 18:40:09 +0100

    fixed poison clearing issue, removed unused conditions for poison trapping

--- target/cheri-common/op_helper_cheri_common.c
+++ target/cheri-common/op_helper_cheri_common.c
@@ -1409,7 +1409,7 @@ void CHERI_HELPER_IMPL(cpoison(CPUArchState *env, uint32_t valreg,
         raise_unaligned_load_exception(env, addr, _host_return_address);
     }
     //printf("cpoison addr end %lx\n", (long) addr);
-    store_cap_to_memory(env, 0, addr, _host_return_address, true);
+    //store_cap_to_memory(env, 0, addr, _host_return_address, true);
 
     //const target_ulong checked_addr =
     //    cap_check_common_reg(perms_for_store(env, valreg), env, authreg, addr,
