8b480faf7fd38a3940d234e3407522c1dffefd59
Yuecheng Wang <yw737@cl.cam.ac.uk>
2025-09-10 12:33:20 +0100

    adding changes for support POISON_PERM bit, and set cap POISON perm instruction

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -82,6 +82,11 @@ static inline target_ulong cap_get_all_perms(const cap_register_t *c)
     return CAP_cc(get_all_permissions)(c);
 }
 
+static inline bool cap_get_poison(const cap_register_t *c)
+{
+    return CAP_cc(get_poison)(c);
+}
+
 static inline void cap_set_perms(cap_register_t *c, target_ulong perms)
 {
     bool success = CAP_cc(set_permissions)(c, perms);
