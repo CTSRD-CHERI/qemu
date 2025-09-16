88e1b26001dbd47f5efbde7d320fe79cc3447098
Paul Buxton <paul.buxton@codasip.com>
2025-09-16 14:54:03 +0100

    Add zbkb extension to A730 support

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -255,6 +255,7 @@ static void rv64_codasip_a730_cpu_init(Object *obj)
     qdev_prop_set_bit(DEVICE(obj), "zcb", true);
     qdev_prop_set_bit(DEVICE(obj), "zcd", true);
     qdev_prop_set_bit(DEVICE(obj), "zcf", true);
+    qdev_prop_set_bit(DEVICE(obj), "zbkb", true);
     /*
      * QEMU 6.x has no support for limiting the virtual addressing modes
      * (later versions add support for filtering certain SvXX modes)
