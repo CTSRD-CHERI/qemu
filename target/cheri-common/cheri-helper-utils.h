920dd282a50abda6c6d11ac2a9c7f0ee088803d5
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-04-15 08:22:44 +0100

    poison: Fix poison check in cap_check_common_reg.
    The tag should be checked on the capability register.
    Disable unused code to silence warnings.

--- target/cheri-common/cheri-helper-utils.h
+++ target/cheri-common/cheri-helper-utils.h
@@ -385,36 +385,35 @@ static inline QEMU_ALWAYS_INLINE target_ulong cap_check_common_reg(
     MMUAccessType rw = required_perms & CAP_PERM_STORE ? MMU_DATA_STORE : MMU_DATA_LOAD;
     
     unsigned int mask = ~((1 << 0) | (1 << 2) | (1 << 3)| (1 << 4));
-
-    //if (((long long)cursor == 0x1234567812345678)&& cpu_in_user_mode(env) && cap_poison &&cheri_in_capmode(env)) {
     
     if (cheri_poison_check(env, addr, size, rw, _host_return_address)) {
         
-        if(!cap_perm_poison && cheri_gettag(cbp)){
-            void *host = probe_read(env, addr&mask, CHERI_CAP_SIZE, cpu_mmu_index(env, false), _host_return_address);
-            target_ulong cursor =0;
-            target_ulong pesbt =0;
+        if (!cap_perm_poison && cbp->cr_tag) {
+#if 0
+            void *host = probe_read(env, addr & mask, CHERI_CAP_SIZE,
+                cpu_mmu_index(env, false), _host_return_address);
+            target_ulong cursor = 0;
+            target_ulong pesbt = 0;
             if (likely(host)) {
                 pesbt = ldq_p((char *)host + CHERI_MEM_OFFSET_METADATA) ^
-                        CC128_NULL_XOR_MASK;
-                //printf("qemu poison check pesbt %lx\n", (long) pesbt);
+                    CC128_NULL_XOR_MASK;
                 cursor = ldq_p((char *)host + CHERI_MEM_OFFSET_CURSOR);
-                //printf("qemu poison check cursor %lx\n", (long) cursor);
             }
             cap_register_t poison_cap;
             CAP_cc(decompress_raw)(pesbt, cursor, 1, &poison_cap);
             cap_length_t poison_top = cap_get_top(&poison_cap);
             target_ulong poison_base = cap_get_base(&poison_cap);
 
-            bool isPoisonCap = false; 
 #if defined(TARGET_RISCV64) && defined(TARGET_CHERI_RISCV_V9)
-            isPoisonCap = cap_get_poison(&poison_cap);
+            bool is_poison_cap = false;
+            is_poison_cap = cap_get_poison(&poison_cap);
 #endif 
-            printf("poison exceptin\n");
+            printf("poison exception\n");
+#endif
             raise_cheri_exception_addr_wnr(env, CapEx_SealViolation, cb, addr,
-                                     !is_load);
+                !is_load);
             /*
-            if(isPoisonCap){
+            if(is_poison_cap){
                 cap_length_t cbp_top = cap_get_top(cbp);
                 target_ulong cbp_base = cap_get_base(cbp);
 
